e6b7b0bf3a9b5a89f2db23baf15953bb441dccfd
thequyenle <quyenle26082001@gmail.com>
2025-11-28 14:12:01 +0700

    fix nhay text khi chinh

--- app/src/main/java/com/piratemaker/postermaker/poster/activity_app/makescreen/MakeScreenActivity.kt
+++ app/src/main/java/com/piratemaker/postermaker/poster/activity_app/makescreen/MakeScreenActivity.kt
@@ -602,6 +602,14 @@ class MakeScreenActivity : BaseActivity<ActivityMakeScreenBinding>() {
             tvBounty?.setTextColor(Color.BLACK)
         }
 
+        // Apply font typeface to tvName
+        val fontName = viewModel.nameFont.value
+        val fontResId = mapFontNameToResource(fontName)
+        if (fontResId != null) {
+            val typeface = androidx.core.content.res.ResourcesCompat.getFont(this, fontResId)
+            tvName?.typeface = typeface
+        }
+
         // Name effects
         tvName?.apply {
             letterSpacing = viewModel.nameSpacing.value
@@ -630,7 +638,6 @@ class MakeScreenActivity : BaseActivity<ActivityMakeScreenBinding>() {
                 }
             }
         }
-        // TODO: Apply font typeface (need to map font name to resource)
 
         // Bounty effects (override config values with user's customization)
         tvBounty?.textSize = viewModel.bountySize.value
@@ -891,6 +898,62 @@ class MakeScreenActivity : BaseActivity<ActivityMakeScreenBinding>() {
 
     private fun showToast(message: String) {
         android.widget.Toast.makeText(this, message, android.widget.Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Map font name to font resource ID
+     * MUST MATCH the font list in WantedEditorActivity
+     */
+    private fun mapFontNameToResource(fontName: String): Int? {
+        return when (fontName) {
+            // Basic fonts
+            "Roboto Bold" -> R.font.roboto_bold
+            "Roboto Medium" -> R.font.roboto_medium
+            "Roboto Regular" -> R.font.roboto_regular
+            "Londrina Solid" -> R.font.londrina_solid_regular
+            "Montserrat Bold" -> R.font.montserrat_bold
+            "Montserrat Medium" -> R.font.montserrat_medium
+
+            // Script/Handwriting (9 fonts)
+            "Script Elegant 1" -> R.font.script_elegant_01
+            "Script Elegant 2" -> R.font.script_elegant_02
+            "Handwriting 1" -> R.font.script_handwriting_01
+            "Handwriting 2" -> R.font.script_handwriting_02
+            "Script Casual" -> R.font.script_casual
+            "Script Bold" -> R.font.script_bold_01
+            "Script Swash" -> R.font.script_swash_01
+            "Script Swirl" -> R.font.script_swirl
+            "Brush Style" -> R.font.brush_01
+
+            // Horror/Gothic/Halloween (8 fonts)
+            "Horror Style 1" -> R.font.display_horror_01
+            "Horror Style 2" -> R.font.display_horror_02
+            "Horror Style 3" -> R.font.display_horror_04
+            "Horror Style 4" -> R.font.display_horror_06
+            "Spooky" -> R.font.display_spooky
+            "Halloween" -> R.font.display_halloween
+            "Gothic" -> R.font.display_gothic_01
+            "Horror Style 5" -> R.font.display_horror_11
+
+            // Display/Decorative (8 fonts)
+            "Tech" -> R.font.display_tech
+            "Tech 3D" -> R.font.display_tech_3d
+            "Tech Outline" -> R.font.display_tech_outline
+            "Tech Gradient" -> R.font.display_tech_gradient
+            "Creative 1" -> R.font.display_creative_01
+            "Creative 2" -> R.font.display_creative_02
+            "Rounded" -> R.font.display_rounded
+            "Decorative" -> R.font.decorative_01
+
+            // Serif Elegant (5 fonts)
+            "Serif Elegant" -> R.font.serif_elegant_01
+            "Serif Elegant Italic" -> R.font.serif_elegant_01_italic
+            "Serif Classic" -> R.font.serif_02
+            "Signature" -> R.font.serif_signature
+            "Halloween Decorative" -> R.font.decorative_halloween_01
+
+            else -> null  // Return null for unknown fonts
+        }
     }
 }
 
