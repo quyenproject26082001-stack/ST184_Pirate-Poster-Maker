df213f29b84f39c08ec9ec4cbdcabfe5d62b3d54
thequyenle <quyenle26082001@gmail.com>
2025-12-05 17:18:09 +0700

    attach ads

--- app/src/main/java/com/piratemaker/postermaker/poster/activity_app/App.kt
+++ app/src/main/java/com/piratemaker/postermaker/poster/activity_app/App.kt
@@ -2,5 +2,26 @@ package com.piratemaker.postermaker.poster.activity_app
 
 import android.app.Application
 
-class App : Application() {
+class App : AdsApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        AppOpenManager.getInstance().disableAppResumeWithActivity(SplashActivity::class.java)
+    }
+
+    override fun enableAdsResume(): Boolean {
+        return true
+    }
+
+    override fun getListTestDeviceId(): MutableList<String>? {
+        return null
+    }
+
+    override fun getResumeAdId(): String {
+        return getString(R.string.open_resume)
+    }
+
+    override fun buildDebug(): Boolean {
+        return true
+    }
 }
+
